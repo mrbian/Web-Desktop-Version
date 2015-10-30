2a3a12ebcb170c69b4c0fe8df4261248e6b0c8ea
卞留念 <554565808@qq.com>
2015-10-30 17:26:48 +0800

    添加两道题

--- TextEditor/js/main.js
+++ TextEditor/js/main.js
@@ -23,3 +23,43 @@ function NaiveStrMatching(T,P){
     else
         return false;
 }
+
+function Strcmp(s,t){
+    if(s.length > t.length){
+        return 1;
+    }else if(s.length < t.length){
+        return -1;
+    }else if(s.length == t.length){
+        return 0;
+    }
+}
+
+
+function FindPos(T,P){
+    var p = P.length - 1;
+    var t = T.length - 1;
+    var plen = P.length;
+    var tlen = T.length;
+    if(tlen < plen)
+        return false;
+    while(p > 0  && t > 0 ){
+        if(T[t] == P[p]){
+            p--;
+            t--;
+        }else{
+            t = t - p;
+            p = P.length - 1;
+        }
+    }
+    if(p <= plen){
+        return t-plen + 1;
+    }
+    else
+        return false;
+}
+
+T = "abbcdd";
+P = "ddfasdff";
+
+console.log(FindPos(T,P));
+console.log(Strcmp(T,P));
